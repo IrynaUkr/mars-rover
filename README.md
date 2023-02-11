e6929ac271fcb3489bf18e584752998a092d5dde
irinapodoprigora <ukrira@gmail.com>
2023-02-11 08:58:15 +0000

    refactored, created RoverAppService.It is responsible for serving app logic

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ Mars Rover depends on:
 * RoverPosition(class helps easy store data coordinates and direction where the rover is facing)
 When rover moves, it takes direction string and spins if there are L or R and go forward in facing direction
 
-To make a move Rover should have methods turnLeft, turnRight and goForward.
+To make a move Rover has  turnLeft, turnRight and goForward methods.
+* The turnLeft method spins the Rover 90 degrees Left without moving from the current coordinate point
+* The turnRight method Spins the Rover 90 degrees Right without moving from the current coordinate point
+* The goForward method Moves the Rover forward by one grid point, maintaining the same orientation
 
-The methods that serve the rover are in the MarsServiceClass class, which corresponds to the Single Responsibility Principle.
+_The methods that serve the rover are in the MarsServiceClass class, which corresponds to the Single Responsibility Principle._
--- src/main/java/org/rover/RoverApp.java
+++ src/main/java/org/rover/RoverApp.java
@@ -1,8 +1,6 @@
 package org.rover;
 
-import org.rover.exception.IncorrectPositionException;
 import org.rover.model.MarsRover;
-import org.rover.model.Orientation;
 import org.rover.model.RectPlateau;
 import org.rover.model.RoverPosition;
 import org.rover.service.MarsRoverService;
@@ -10,6 +8,8 @@ import org.rover.service.MarsRoverService;
 import java.util.Scanner;
 
 import static java.lang.System.in;
+import static org.rover.service.RoverAppService.createPlateau;
+import static org.rover.service.RoverAppService.createRoverPosition;
 
 public class RoverApp {
     public static void main(String[] args) {
@@ -19,37 +19,20 @@ public class RoverApp {
         String plateauCoordinates = scanner.nextLine();
         RectPlateau plateau = createPlateau(plateauCoordinates);
 
+
         System.out.println("Enter coordinates and facing of the  rover (in the form x y N):");
         String roverPositionData = scanner.nextLine();
         RoverPosition roverPosition = createRoverPosition(roverPositionData);
         MarsRover rover = new MarsRover(plateau, roverPosition);
 
+
         System.out.println("Enter Directions without spaces(in the form LMRLMR)");
         String directions = scanner.nextLine();
 
-      MarsRoverService roverService = new MarsRoverService(rover);
+        MarsRoverService roverService = new MarsRoverService(rover);
         RoverPosition move = roverService.move(directions);
         System.out.println(move);
 
-
-    }
-
-    private static RectPlateau createPlateau(String plateauCoordinates) {
-        if (plateauCoordinates.matches("\\d\\s\\d")) {
-            int platoX = plateauCoordinates.charAt(0) - '0';
-            int platoY = plateauCoordinates.charAt(2) - '0';
-            return new RectPlateau(platoX, platoY);
-        } else throw new IllegalArgumentException();
-    }
-
-    private static RoverPosition createRoverPosition(String plateauCoordinates) {
-        if (plateauCoordinates.matches("\\d\\s\\d\\s['N|S|W|E']")) {
-            int roverX = plateauCoordinates.charAt(0) - '0';
-            int roverY = plateauCoordinates.charAt(2) - '0';
-            Orientation orientation = Orientation.valueOf(plateauCoordinates.charAt(4) + "");
-            return new RoverPosition(roverX, roverY, orientation);
-        }
-        throw new IncorrectPositionException("The string of Rover position is incorrect");
     }
 
 }
